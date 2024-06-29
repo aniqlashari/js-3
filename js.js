e25c663681b1dd355eee6918c728a0e5deb46107
aniqlashari <mahaqamber3@gmail.com>
2024-06-29 20:53:01 +0500

    first commit

---
+++ js.js
@@ -0,0 +1,21 @@
+
+var name = prompt ("kashan@gmail.com")
+
+var password = prompt("kashan")
+
+if (name == "kashan@gmail.com") {
+    alert("you are kashan")
+
+}
+ 
+else if (password == "kashan") {
+    alert("welcome  back   kashan")
+}
+
+
+else{
+
+alert ("you are admin");
+
+
+}
